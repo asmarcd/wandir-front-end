e920aedd7e21db0c70045c33eee04d19952c63ac
Sean Morgan <insideseanshead@gmail.com>
2020-11-12 23:51:06 -0800

    imported css and jquery to run lightbox and filled photo page with examples.

--- src/pages/Photos/index.js
+++ src/pages/Photos/index.js
@@ -55,6 +55,102 @@ export default function Photos() {
             alt=""
           />
         </a>
+        <a
+          class="example-image-link"
+          href="http://lokeshdhakar.com/projects/lightbox2/images/image-3.jpg"
+          data-lightbox="example-set"
+          data-title="Click the right half of the image to move forward."
+        >
+          <img
+            class="example-image"
+            src="http://lokeshdhakar.com/projects/lightbox2/images/thumb-3.jpg"
+            alt=""
+          />
+        </a>
+        <a
+          class="example-image-link"
+          href="http://lokeshdhakar.com/projects/lightbox2/images/image-4.jpg"
+          data-lightbox="example-set"
+          data-title="Or press the right arrow on your keyboard."
+        >
+          <img
+            class="example-image"
+            src="http://lokeshdhakar.com/projects/lightbox2/images/thumb-4.jpg"
+            alt=""
+          />
+        </a>
+        <a
+          class="example-image-link"
+          href="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+          data-lightbox="example-set"
+          data-title="The next image in the set is preloaded as you're viewing."
+        >
+          <img
+            class="example-image"
+            src="http://lokeshdhakar.com/projects/lightbox2/images/thumb-5.jpg"
+            alt=""
+          />
+        </a>
+        <a
+          class="example-image-link"
+          href="http://lokeshdhakar.com/projects/lightbox2/images/image-6.jpg"
+          data-lightbox="example-set"
+          data-title="Click anywhere outside the image or the X to the right to close."
+        >
+          <img
+            class="example-image"
+            src="http://lokeshdhakar.com/projects/lightbox2/images/thumb-6.jpg"
+            alt=""
+          />
+        </a>
+        <a
+          class="example-image-link"
+          href="http://lokeshdhakar.com/projects/lightbox2/images/image-3.jpg"
+          data-lightbox="example-set"
+          data-title="Click the right half of the image to move forward."
+        >
+          <img
+            class="example-image"
+            src="http://lokeshdhakar.com/projects/lightbox2/images/thumb-3.jpg"
+            alt=""
+          />
+        </a>
+        <a
+          class="example-image-link"
+          href="http://lokeshdhakar.com/projects/lightbox2/images/image-4.jpg"
+          data-lightbox="example-set"
+          data-title="Or press the right arrow on your keyboard."
+        >
+          <img
+            class="example-image"
+            src="http://lokeshdhakar.com/projects/lightbox2/images/thumb-4.jpg"
+            alt=""
+          />
+        </a>
+        <a
+          class="example-image-link"
+          href="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+          data-lightbox="example-set"
+          data-title="The next image in the set is preloaded as you're viewing."
+        >
+          <img
+            class="example-image"
+            src="http://lokeshdhakar.com/projects/lightbox2/images/thumb-5.jpg"
+            alt=""
+          />
+        </a>
+        <a
+          class="example-image-link"
+          href="http://lokeshdhakar.com/projects/lightbox2/images/image-6.jpg"
+          data-lightbox="example-set"
+          data-title="Click anywhere outside the image or the X to the right to close."
+        >
+          <img
+            class="example-image"
+            src="http://lokeshdhakar.com/projects/lightbox2/images/thumb-6.jpg"
+            alt=""
+          />
+        </a>
       </div>
     </div>
   );

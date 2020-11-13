8c2e54360e0b5ab2bc2843b5710248acd5bc2dbd
Sean Morgan <insideseanshead@gmail.com>
2020-11-12 23:43:27 -0800

    installed npm package lightbox2

--- src/pages/Photos/index.js
+++ src/pages/Photos/index.js
@@ -1,28 +1,60 @@
 import React from "react";
 import "./style.css";
+import "lightbox2/dist/css/lightbox.min.css";
+import "lightbox2/dist/js/lightbox-plus-jquery.min.js";
 
 export default function Photos() {
   return (
     <div id="photoWindow">
       <div id="gallery">
-        <img src="https://www.placecage.com/c/300/300" />
-        <img src="https://www.placecage.com/c/300/300" />
-        <img src="https://www.placecage.com/c/300/300" />
-        <img src="https://www.placecage.com/c/300/300" />
-        <img src="https://www.placecage.com/c/300/300" />
-        <img src="https://www.placecage.com/c/300/300" />
-        <img src="https://www.placecage.com/c/300/300" />
-        <img src="https://www.placecage.com/c/300/300" />
-        <img src="https://www.placecage.com/c/300/300" />
-        <img src="https://www.placecage.com/c/300/300" />
-        <img src="https://www.placecage.com/c/300/300" />
-        <img src="https://www.placecage.com/c/300/300" />
-        <img src="https://www.placecage.com/c/300/300" />
-        <img src="https://www.placecage.com/c/300/300" />
-        <img src="https://www.placecage.com/c/300/300" />
-        <img src="https://www.placecage.com/c/300/300" />
-        <img src="https://www.placecage.com/c/300/300" />
-        <img src="https://www.placecage.com/c/300/300" />
+        <a
+          class="example-image-link"
+          href="http://lokeshdhakar.com/projects/lightbox2/images/image-3.jpg"
+          data-lightbox="example-set"
+          data-title="Click the right half of the image to move forward."
+        >
+          <img
+            class="example-image"
+            src="http://lokeshdhakar.com/projects/lightbox2/images/thumb-3.jpg"
+            alt=""
+          />
+        </a>
+        <a
+          class="example-image-link"
+          href="http://lokeshdhakar.com/projects/lightbox2/images/image-4.jpg"
+          data-lightbox="example-set"
+          data-title="Or press the right arrow on your keyboard."
+        >
+          <img
+            class="example-image"
+            src="http://lokeshdhakar.com/projects/lightbox2/images/thumb-4.jpg"
+            alt=""
+          />
+        </a>
+        <a
+          class="example-image-link"
+          href="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+          data-lightbox="example-set"
+          data-title="The next image in the set is preloaded as you're viewing."
+        >
+          <img
+            class="example-image"
+            src="http://lokeshdhakar.com/projects/lightbox2/images/thumb-5.jpg"
+            alt=""
+          />
+        </a>
+        <a
+          class="example-image-link"
+          href="http://lokeshdhakar.com/projects/lightbox2/images/image-6.jpg"
+          data-lightbox="example-set"
+          data-title="Click anywhere outside the image or the X to the right to close."
+        >
+          <img
+            class="example-image"
+            src="http://lokeshdhakar.com/projects/lightbox2/images/thumb-6.jpg"
+            alt=""
+          />
+        </a>
       </div>
     </div>
   );

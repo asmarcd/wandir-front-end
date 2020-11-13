a713ae2b914dd402c8886f25b6f02fd740699524
Sean Morgan <insideseanshead@gmail.com>
2020-11-13 12:30:48 -0800

    updated gallery so that thumbnails scale and grid out using bulma columns

--- src/pages/Photos/index.js
+++ src/pages/Photos/index.js
@@ -2,12 +2,481 @@ import React from "react";
 import "./style.css";
 import "lightbox2/dist/css/lightbox.min.css";
 import "lightbox2/dist/js/lightbox-plus-jquery.min.js";
+import "react-bulma-components/dist/react-bulma-components.min.css";
 
 export default function Photos() {
   return (
     <div id="photoWindow">
       <div id="gallery">
-        <a
+        <div class="columns is-multiline">
+        <div class="column is-4">
+          <a
+            class="example-image-link"
+            href="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+            data-lightbox="example-set"
+            data-title="The next image in the set is preloaded as you're viewing."
+          >
+            <figure class="image is-square">
+              <img
+                class="example-image"
+                src="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+                alt=""
+              />
+            </figure>
+          </a>
+        </div>
+        <div class="column is-4">
+          <a
+            class="example-image-link"
+            href="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+            data-lightbox="example-set"
+            data-title="The next image in the set is preloaded as you're viewing."
+          >
+            <figure class="image is-square">
+              <img
+                class="example-image"
+                src="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+                alt=""
+              />
+            </figure>
+          </a>
+        </div>
+        <div class="column is-4">
+          <a
+            class="example-image-link"
+            href="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+            data-lightbox="example-set"
+            data-title="The next image in the set is preloaded as you're viewing."
+          >
+            <figure class="image is-square">
+              <img
+                class="example-image"
+                src="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+                alt=""
+              />
+            </figure>
+          </a>
+        </div>
+        <div class="column is-4">
+          <a
+            class="example-image-link"
+            href="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+            data-lightbox="example-set"
+            data-title="The next image in the set is preloaded as you're viewing."
+          >
+            <figure class="image is-square">
+              <img
+                class="example-image"
+                src="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+                alt=""
+              />
+            </figure>
+          </a>
+        </div>
+        <div class="column is-4">
+          <a
+            class="example-image-link"
+            href="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+            data-lightbox="example-set"
+            data-title="The next image in the set is preloaded as you're viewing."
+          >
+            <figure class="image is-square">
+              <img
+                class="example-image"
+                src="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+                alt=""
+              />
+            </figure>
+          </a>
+        </div>
+        <div class="column is-4">
+          <a
+            class="example-image-link"
+            href="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+            data-lightbox="example-set"
+            data-title="The next image in the set is preloaded as you're viewing."
+          >
+            <figure class="image is-square">
+              <img
+                class="example-image"
+                src="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+                alt=""
+              />
+            </figure>
+          </a>
+        </div>
+        <div class="column is-4">
+          <a
+            class="example-image-link"
+            href="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+            data-lightbox="example-set"
+            data-title="The next image in the set is preloaded as you're viewing."
+          >
+            <figure class="image is-square">
+              <img
+                class="example-image"
+                src="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+                alt=""
+              />
+            </figure>
+          </a>
+        </div>
+        <div class="column is-4">
+          <a
+            class="example-image-link"
+            href="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+            data-lightbox="example-set"
+            data-title="The next image in the set is preloaded as you're viewing."
+          >
+            <figure class="image is-square">
+              <img
+                class="example-image"
+                src="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+                alt=""
+              />
+            </figure>
+          </a>
+        </div>
+        <div class="column is-4">
+          <a
+            class="example-image-link"
+            href="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+            data-lightbox="example-set"
+            data-title="The next image in the set is preloaded as you're viewing."
+          >
+            <figure class="image is-square">
+              <img
+                class="example-image"
+                src="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+                alt=""
+              />
+            </figure>
+          </a>
+        </div>
+        <div class="column is-4">
+          <a
+            class="example-image-link"
+            href="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+            data-lightbox="example-set"
+            data-title="The next image in the set is preloaded as you're viewing."
+          >
+            <figure class="image is-square">
+              <img
+                class="example-image"
+                src="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+                alt=""
+              />
+            </figure>
+          </a>
+        </div>
+        <div class="column is-4">
+          <a
+            class="example-image-link"
+            href="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+            data-lightbox="example-set"
+            data-title="The next image in the set is preloaded as you're viewing."
+          >
+            <figure class="image is-square">
+              <img
+                class="example-image"
+                src="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+                alt=""
+              />
+            </figure>
+          </a>
+        </div>
+        <div class="column is-4">
+          <a
+            class="example-image-link"
+            href="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+            data-lightbox="example-set"
+            data-title="The next image in the set is preloaded as you're viewing."
+          >
+            <figure class="image is-square">
+              <img
+                class="example-image"
+                src="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+                alt=""
+              />
+            </figure>
+          </a>
+        </div>
+        <div class="column is-4">
+          <a
+            class="example-image-link"
+            href="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+            data-lightbox="example-set"
+            data-title="The next image in the set is preloaded as you're viewing."
+          >
+            <figure class="image is-square">
+              <img
+                class="example-image"
+                src="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+                alt=""
+              />
+            </figure>
+          </a>
+        </div>
+        <div class="column is-4">
+          <a
+            class="example-image-link"
+            href="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+            data-lightbox="example-set"
+            data-title="The next image in the set is preloaded as you're viewing."
+          >
+            <figure class="image is-square">
+              <img
+                class="example-image"
+                src="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+                alt=""
+              />
+            </figure>
+          </a>
+        </div>
+        <div class="column is-4">
+          <a
+            class="example-image-link"
+            href="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+            data-lightbox="example-set"
+            data-title="The next image in the set is preloaded as you're viewing."
+          >
+            <figure class="image is-square">
+              <img
+                class="example-image"
+                src="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+                alt=""
+              />
+            </figure>
+          </a>
+        </div>
+        <div class="column is-4">
+          <a
+            class="example-image-link"
+            href="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+            data-lightbox="example-set"
+            data-title="The next image in the set is preloaded as you're viewing."
+          >
+            <figure class="image is-square">
+              <img
+                class="example-image"
+                src="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+                alt=""
+              />
+            </figure>
+          </a>
+        </div>
+        <div class="column is-4">
+          <a
+            class="example-image-link"
+            href="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+            data-lightbox="example-set"
+            data-title="The next image in the set is preloaded as you're viewing."
+          >
+            <figure class="image is-square">
+              <img
+                class="example-image"
+                src="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+                alt=""
+              />
+            </figure>
+          </a>
+        </div>
+        <div class="column is-4">
+          <a
+            class="example-image-link"
+            href="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+            data-lightbox="example-set"
+            data-title="The next image in the set is preloaded as you're viewing."
+          >
+            <figure class="image is-square">
+              <img
+                class="example-image"
+                src="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+                alt=""
+              />
+            </figure>
+          </a>
+        </div>
+        <div class="column is-4">
+          <a
+            class="example-image-link"
+            href="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+            data-lightbox="example-set"
+            data-title="The next image in the set is preloaded as you're viewing."
+          >
+            <figure class="image is-square">
+              <img
+                class="example-image"
+                src="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+                alt=""
+              />
+            </figure>
+          </a>
+        </div>
+        <div class="column is-4">
+          <a
+            class="example-image-link"
+            href="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+            data-lightbox="example-set"
+            data-title="The next image in the set is preloaded as you're viewing."
+          >
+            <figure class="image is-square">
+              <img
+                class="example-image"
+                src="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+                alt=""
+              />
+            </figure>
+          </a>
+        </div>
+        <div class="column is-4">
+          <a
+            class="example-image-link"
+            href="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+            data-lightbox="example-set"
+            data-title="The next image in the set is preloaded as you're viewing."
+          >
+            <figure class="image is-square">
+              <img
+                class="example-image"
+                src="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+                alt=""
+              />
+            </figure>
+          </a>
+        </div>
+        <div class="column is-4">
+          <a
+            class="example-image-link"
+            href="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+            data-lightbox="example-set"
+            data-title="The next image in the set is preloaded as you're viewing."
+          >
+            <figure class="image is-square">
+              <img
+                class="example-image"
+                src="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+                alt=""
+              />
+            </figure>
+          </a>
+        </div>
+        <div class="column is-4">
+          <a
+            class="example-image-link"
+            href="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+            data-lightbox="example-set"
+            data-title="The next image in the set is preloaded as you're viewing."
+          >
+            <figure class="image is-square">
+              <img
+                class="example-image"
+                src="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+                alt=""
+              />
+            </figure>
+          </a>
+        </div>
+        <div class="column is-4">
+          <a
+            class="example-image-link"
+            href="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+            data-lightbox="example-set"
+            data-title="The next image in the set is preloaded as you're viewing."
+          >
+            <figure class="image is-square">
+              <img
+                class="example-image"
+                src="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+                alt=""
+              />
+            </figure>
+          </a>
+        </div>
+        <div class="column is-4">
+          <a
+            class="example-image-link"
+            href="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+            data-lightbox="example-set"
+            data-title="The next image in the set is preloaded as you're viewing."
+          >
+            <figure class="image is-square">
+              <img
+                class="example-image"
+                src="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+                alt=""
+              />
+            </figure>
+          </a>
+        </div>
+        <div class="column is-4">
+          <a
+            class="example-image-link"
+            href="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+            data-lightbox="example-set"
+            data-title="The next image in the set is preloaded as you're viewing."
+          >
+            <figure class="image is-square">
+              <img
+                class="example-image"
+                src="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+                alt=""
+              />
+            </figure>
+          </a>
+        </div>
+        <div class="column is-4">
+          <a
+            class="example-image-link"
+            href="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+            data-lightbox="example-set"
+            data-title="The next image in the set is preloaded as you're viewing."
+          >
+            <figure class="image is-square">
+              <img
+                class="example-image"
+                src="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+                alt=""
+              />
+            </figure>
+          </a>
+        </div>
+        <div class="column is-4">
+          <a
+            class="example-image-link"
+            href="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+            data-lightbox="example-set"
+            data-title="The next image in the set is preloaded as you're viewing."
+          >
+            <figure class="image is-square">
+              <img
+                class="example-image"
+                src="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+                alt=""
+              />
+            </figure>
+          </a>
+        </div>
+        <div class="column is-4">
+          <a
+            class="example-image-link"
+            href="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+            data-lightbox="example-set"
+            data-title="The next image in the set is preloaded as you're viewing."
+          >
+            <figure class="image is-square">
+              <img
+                class="example-image"
+                src="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
+                alt=""
+              />
+            </figure>
+          </a>
+        </div>
+
+
+        </div>
+        {/* <a
           class="example-image-link"
           href="http://lokeshdhakar.com/projects/lightbox2/images/image-3.jpg"
           data-lightbox="example-set"
@@ -15,7 +484,7 @@ export default function Photos() {
         >
           <img
             class="example-image"
-            src="http://lokeshdhakar.com/projects/lightbox2/images/thumb-3.jpg"
+            src="http://lokeshdhakar.com/projects/lightbox2/images/image-3.jpg"
             alt=""
           />
         </a>
@@ -24,13 +493,16 @@ export default function Photos() {
           href="http://lokeshdhakar.com/projects/lightbox2/images/image-4.jpg"
           data-lightbox="example-set"
           data-title="Or press the right arrow on your keyboard."
-        >
+        ><figure class ="image is-square">
           <img
             class="example-image"
             src="http://lokeshdhakar.com/projects/lightbox2/images/thumb-4.jpg"
             alt=""
           />
+          </figure>
         </a>
+        
+        
         <a
           class="example-image-link"
           href="http://lokeshdhakar.com/projects/lightbox2/images/image-5.jpg"
@@ -38,11 +510,14 @@ export default function Photos() {
           data-title="The next image in the set is preloaded as you're viewing."
         >
           <img
-            class="example-image"
+            class="example-image image is-square"
             src="http://lokeshdhakar.com/projects/lightbox2/images/thumb-5.jpg"
             alt=""
           />
         </a>
+
+
+
         <a
           class="example-image-link"
           href="http://lokeshdhakar.com/projects/lightbox2/images/image-6.jpg"
@@ -150,7 +625,7 @@ export default function Photos() {
             src="http://lokeshdhakar.com/projects/lightbox2/images/thumb-6.jpg"
             alt=""
           />
-        </a>
+        </a> */}
       </div>
     </div>
   );

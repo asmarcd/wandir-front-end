f6f4a3ac332324f5da81d4a10234d4e3623c7af9
Hilary Valencia-Walsh <hilaryvalenciawalsh@gmail.com>
2020-11-13 11:43:41 -0800

    Journal

--- src/pages/Journal/index.js
+++ src/pages/Journal/index.js
@@ -4,7 +4,71 @@ import "./style.css"
 export default function Journal() {
     return (
         <div id="journalWindow">
-            <h2>Journal Window</h2>
+            
+            <article className="media">
+       
+        <div className="media-content">
+          <div className="content">
+            <p>
+              <strong>Starting my Journey to the Desert</strong> 
+              <br />
+              <small>11/13/2020</small>
+              <br />
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.
+            </p>
+          </div>
+        </div>
+       
+      </article>
+      <br />
+      <article className="media">
+       
+       <div className="media-content">
+         <div className="content">
+           <p>
+             <strong>Walking...</strong> 
+             <br />
+             <small>11/13/2020</small> 
+             <br />
+             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.
+           </p>
+         </div>
+       </div>
+      
+     </article>
+     <br />
+     <article className="media">
+       
+       <div className="media-content">
+         <div className="content">
+           <p>
+             <strong>No water.. i need water</strong> 
+             <br />
+             <small>11/13/2020</small>
+             <br />
+             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.
+           </p>
+         </div>
+       </div>
+      <br />
+      
+     </article>
+     <article className="media">
+       
+       <div className="media-content">
+         <div className="content">
+           <p>
+             <strong>Yay! We made it!</strong> 
+             <br />
+             <small>11/13/2020</small>
+             <br />
+             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.
+           </p>
+         </div>
+       </div>
+      <br />
+      
+     </article>
         </div>
     )
 }

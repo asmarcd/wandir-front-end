6688f46f43031650dae18aebd61c1486db998efb
Sean Morgan <insideseanshead@gmail.com>
2020-11-14 22:04:04 -0800

    updated hero and added user log out button. Updated component sizes for full screen

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,29 +1,41 @@
 import React from "react";
-import "./style.css"
+import "./style.css";
+import Logo from "../../assets/logo.png";
 
 export default function Hero() {
   return (
     <div>
-      <section className="hero is-medium is-bold">
+      <section className="heroImg">
         <div className="hero-body">
           <div className="container">
-            <h1>Wondir</h1>
-            <h2>Subtitle Here!</h2>
+            
+            <img className="brandLogo" src={Logo} alt="logo" />
+            
+            <div className="columns" id="userMenus">
+              <div className="column 4">
+                <div className="field has-addons">
+                  <div className="control">
+                    <input
+                      className="input"
+                      type="text"
+                      placeholder="Find a repository"
+                    />
+                  </div>
+                  <div className="control">
+                    <a className="button is-warning">Search</a>
+                  </div>
+                  <br />
+                </div>
+              </div>
+              <div className="column 4">
+              <div className="control is-pulled-right">
+                    <a className="button is-warning ">Log Out</a>
+                  </div>
+              </div>
+            </div>
           </div>
-        </div> <div className="field has-addons">
-        <div className="control">
-          <input className="input" type="text" placeholder="Find a repository" />
         </div>
-        <div className="control">
-          <a className="button is-warning">
-            Search
-          </a>
-        </div>
-        <br />
-      </div>
       </section>
-     
-   
     </div>
   );
 }

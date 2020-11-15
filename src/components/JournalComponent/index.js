88ab21582cc6ea953b4a1cc7a03af02b355f5b01
Sean Morgan <insideseanshead@gmail.com>
2020-11-14 23:47:47 -0800

    collapseable journal entries

--- src/components/JournalComponent/index.js
+++ src/components/JournalComponent/index.js
@@ -1,19 +1,52 @@
 import React from "react";
+import "./style.css"
+
+
+
+function expand(){
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
+}
 
 export default function JournalComponent({ id, title, date, body, ...rest }) {
   return (
-      <article className="media" {...rest}>
+    <article className="media" {...rest}>
         <div className="media-content">
+          <button className="collapsible" onClick={expand}><strong>{title}</strong> <small>{date}</small></button>
           <div className="content">
             <p>
-              <strong>{title}</strong>
-              <br />
-              <small>{date}</small>
-              <br />
               {body}
             </p>
           </div>
         </div>
-      </article>
+      </article>  
+    
+  
+    
+    // <article className="media" {...rest}>
+      //   <div className="media-content">
+      //     <div className="content">
+      //       <p>
+      //         <strong>{title}</strong>
+      //         <br />
+      //         <small>{date}</small>
+      //         <br />
+      //         {body}
+      //       </p>
+      //     </div>
+      //   </div>
+      // </article>
   );
 }

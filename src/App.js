5fa728daa2db32d3ce696a6eb46d61a45cca5347
Hilary Valencia-Walsh <hilaryvalenciawalsh@gmail.com>
2020-11-17 11:14:01 -0800

    routes

--- src/App.js
+++ src/App.js
@@ -11,12 +11,13 @@ import Photos from "./components/Photos";
 import Footer from "./components/Footer";
 import API from "./utils/API";
 import GeoStateContext from "./contexts/GeoStateContext";
+import LandingPage from "./components/LandingPage";
 
 function App() {
   const [geoState, setGeoState] = useState([]);
   const [journalEntries, setJournalEntries] = useState([]);
   const [photos, setPhotos] = useState([]);
-  const [viewState, setViewState] = useState("journal")
+  const [viewState, setViewState] = useState("journal");
   //
   const [userState, setUserState] = useState({
     id: 1,
@@ -27,86 +28,132 @@ function App() {
   });
 
   useEffect(() => {
-    handleFilterContent(0,"all")
-    
+    handleFilterContent(0, "all");
   }, []);
 
-  const handleViewSwitch = (event) =>{
-    console.log(event)
-    if(event.target.id==="journalBtn"){
-      setViewState("journal")
-    }else if(event.target.id==="photoBtn"){
-      setViewState("button")
+  const handleViewSwitch = (event) => {
+    console.log(event);
+    if (event.target.id === "journalBtn") {
+      setViewState("journal");
+    } else if (event.target.id === "photoBtn") {
+      setViewState("button");
     }
-  }
+  };
   // const updateGeoFnc =(newGeo,id) =>{
   //   if(id){
   //     console.log()
   //     const geoPop = geoState.filter(e=>e.id!=id)
-  //     setGeoState(geoPop); 
+  //     setGeoState(geoPop);
   //   }else{
   //     setGeoState(geoState => [...geoState, newGeo]);
   //     console.log(newGeo)
   //   }
-    
+
   // }
-  
-  const handleFilterContent = (id, type) =>{
-    if(type==="all"){
+
+  const handleFilterContent = (id, type) => {
+    if (type === "all") {
       API.getUserData(userState.id).then(async (userdata) => {
         await setGeoState(userdata.geo);
-        await setJournalEntries (userdata.entry.map(({id,title,date,body})=>({id,title,date,body})));
-        await setPhotos(userdata.photo.map(({id,url,EntryId:entryId,GeroId:geoId})=>({id,url,entryId,geoId})));
-        console.log(userdata)
-
+        await setJournalEntries(
+          userdata.entry.map(({ id, title, date, body }) => ({
+            id,
+            title,
+            date,
+            body,
+          }))
+        );
+        await setPhotos(
+          userdata.photo.map(
+            ({ id, url, EntryId: entryId, GeroId: geoId }) => ({
+              id,
+              url,
+              entryId,
+              geoId,
+            })
+          )
+        );
+        console.log(userdata);
       });
     }
-    if(type==="geo"){
+    if (type === "geo") {
       API.filterByPoint(id).then((geodata) => {
         // cycle through both the geo and entry records for the included photos
         setGeoState(geodata);
-        if(geodata[0].Entries.length > 0){
-         console.log("true")
-          setJournalEntries (geodata[0].Entries.map(({id,title,date,body})=>({id,title,date,body})));
+        if (geodata[0].Entries.length > 0) {
+          console.log("true");
+          setJournalEntries(
+            geodata[0].Entries.map(({ id, title, date, body }) => ({
+              id,
+              title,
+              date,
+              body,
+            }))
+          );
         }
-        if(geodata[0].Photos.length > 0){
-          console.log("true")
-          setPhotos(geodata[0].Photos.map(({id,url,EntryId:entryId,GeroId:geoId})=>({id,url,entryId,geoId})));
+        if (geodata[0].Photos.length > 0) {
+          console.log("true");
+          setPhotos(
+            geodata[0].Photos.map(
+              ({ id, url, EntryId: entryId, GeroId: geoId }) => ({
+                id,
+                url,
+                entryId,
+                geoId,
+              })
+            )
+          );
         }
-        
       });
     }
     // return null
-    
-  }
+  };
 
   return (
-
-    <GeoStateContext.Provider value={{geoState, journalEntries,photos,userState ,handleFilterContent}}>
-      <div className="App">
-        <Hero />
-        <div class="container">
-          <div class="columns">
-            <div class="column">
+    <GeoStateContext.Provider
+      value={{
+        geoState,
+        journalEntries,
+        photos,
+        userState,
+        handleFilterContent,
+      }}
+    >
+      
+      <Router>
+       <div className="App">
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+             <Route exact path="/Login">
+              <LandingPage />
+            </Route> 
+            <Route path="/">
+            <Hero />
+        <div className="container">
+          <div className="columns">
+            <div className="column">
               <Map />
             </div>
-            <div class="column">
-              <Router>
+            <div className="column">
                 <WindowNav handleViewSwitch={handleViewSwitch}/>
-                <div class="columns">
+                <div className="columns">
                   {/* Router buttons for map and journal */}
-                  <div class="column">
+                  <div className="column">
                     {viewState==="journal"? <Journal /> : <Photos />}
                     {/* <Route exact path="/Journal" component={Journal} />
                     <Route exact path="/photos" component={Photos} /> */}
                   </div>
                 </div>
-              </Router>
+              
             </div>
           </div>
         </div>
         <Footer />
-      </div>
+            </Route>
+          </Switch>
+       </div>
+      </Router>
     </GeoStateContext.Provider>
   );
 }
--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -1,3 +1,62 @@
 import React from "react";
 import "./style.css";
+import Logo from "../../assets/logo.png";
 
+class LandingPage extends React.Component {
+  constructor() {
+    super();
+    
+    this.state = { form: "login" };
+    this.toggle = {
+      login: "register",
+      register: "login",
+    };
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="LandingContainer"><img src={Logo} /> <br />
+        <div
+          style={{
+            transform: `translate(${
+              this.state.form === "login" ? 0 : 100
+            }px, 0px)`,
+          }}
+          className="LandingForm"
+        >
+          <form onSubmit={this.onSubmit.bind(this)}>
+            <input placeholder="Email" type="text" />
+            <input placeholder="Password" type="password" />
+            {this.state.form === "login" ? (
+              ""
+            ) : (
+              <input placeholder="Re-typed password" type="password" />
+            )}
+            <button>Submit</button>
+          </form>
+        </div>
+        <div
+          style={{
+            transform: `translate(${
+              this.state.form === "login" ? 0 : -200
+            }px, 0px)`,
+          }}
+          className="LandingButtonDiv"
+        >
+          <button
+            onClick={() => {
+              this.setState({ form: this.toggle[this.state.form] });
+            }}
+          >
+            {this.toggle[this.state.form]}
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+export default LandingPage

d02d72193829716c3b09aa0dd0825a3b4006f27f
Jonny K <Kempj2.jk@gmail.com>
2020-11-18 15:04:39 -0800

    API route built, passing photo in properly and rendering on the page

--- src/App.js
+++ src/App.js
@@ -163,7 +163,7 @@ function App() {
   }
 
   return (
-    <GeoStateContext.Provider value={{ geoState, journalEntries, photos, inputState, userState, editEntry, handleInputChange, handleFilterContent, deleteReset }}>
+    <GeoStateContext.Provider value={{ geoState, journalEntries, photos, inputState, userState, editEntry, handleInputChange, handleFilterContent, deleteReset, fireRefresh }}>
 
       <Router>
        <div className="App">

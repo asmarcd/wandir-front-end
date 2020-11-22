3eec35d29bad09fd685524da052bbd4786d5cb82
Jonny K <Kempj2.jk@gmail.com>
2020-11-21 17:25:20 -0800

    commenting code

--- src/components/PlaceSearch/index.js
+++ src/components/PlaceSearch/index.js
@@ -5,11 +5,13 @@ import "./style.css";
 import GeoStateContext from "../../contexts/GeoStateContext";
 
 export default function PlaceSearch(props) {
+    // get the latest user state
     const { userState} = useContext(GeoStateContext)
-
+    // setup state for the input and the marker that comes back from the nomination request
     const [input, setInput] = useState({input:"",submit:false})
     const [markerObj, setMarkerObj] = useState()
 
+    // boilerplate handle input
     const handleTextInput = (e) => {
         const name = e.target.name;
         const value = e.target.value;
@@ -18,27 +20,36 @@ export default function PlaceSearch(props) {
           [name]: value,
         });
       };
-
+    
+    // on hitting the search button
     const handleSubmit =(event) =>{
         event.preventDefault()
+        // break out if the user didn't actually enter a query
         if(!input.query){
             return
         }
+        // otherwise go ahead and use the input query against the OSM nomination geolocator
         API.nominationSearch(input.query).then(res=>{
+            // set state to the object
             setMarkerObj({
+                // grab only the beginning of the place name
                 place: res[0].display_name.split(',')[0],
                 region:res[0].address.city,
                 lat: res[0].lat,
                 lng: res[0].lon,
+                // grab the bounds so that you have the right zoom level
                 bounds:[[res[0].boundingbox[0],res[0].boundingbox[2]],[res[0].boundingbox[1],res[0].boundingbox[3]]]
                 })
         }).then(()=>{
+            // reset the input and change submit so the zoomMap component can turn on
             setInput({input:"",submit:true})
         })
     }
+    // handles the Save button click
     const handleClick =()=>{
-        console.log(markerObj)
+        // clear the input fo the geosearch and reste the submit state
         setInput({input:"",submit:false})
+        // fire the handle save function up a level in the map component
         props.handleSave({
             UserId: userState.id,
             place: markerObj.place,
@@ -46,15 +57,18 @@ export default function PlaceSearch(props) {
             lat: markerObj.lat,
             lng: markerObj.lng})
     }
+    // does work to zoom the map and creates the marker
     const ZoomMap = ({ marker }) =>{
-        console.log(marker    )
+        // use the map variable out of leaflet
         const map = useMap()
+        // set he view, an the bounds(zoom) based on the marker
         map.setView([marker.lat, marker.lng])
         map.fitBounds(marker.bounds)
+        // plot the marker
         return (
             <Marker
-            // makes the marker draggable
-            draggable={true}
+            // makes the marker not draggable
+            draggable={false}
             // Setup the handler for the events to this ol gal
             // Not being used, but maybe could style this off it?
             className="pending-marker"
@@ -64,7 +78,7 @@ export default function PlaceSearch(props) {
           >
             {/* THe popup for this little gem */}
             <Popup >
-              <p>{marker.place}</p>
+              <p className="popupPlace">{marker.place}</p>
               {userState.isLoggedIn?(
                 <span>
                 <button onClick={handleClick}>Save</button>
@@ -76,6 +90,7 @@ export default function PlaceSearch(props) {
     }
     
     return (
+        // Return the actual search component for use on the map
         <div className="search">
             {input.submit ? <ZoomMap marker={markerObj}/> : null}
             <form>
